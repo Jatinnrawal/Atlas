f162f02726e22f0dc407a7a74373325ed98bea1d
Jatin Rawal <jatinrawal99999@gmail.com>
2026-08-27 12:10:53 +0000

    feat: add private CloudWatch monitoring endpoint

--- terraform/environments/dev/endpoints.tf
+++ terraform/environments/dev/endpoints.tf
@@ -139,3 +139,26 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
     Project     = "atlas"
   }
 }
+
+# CloudWatch Monitoring VPC Endpoint
+resource "aws_vpc_endpoint" "monitoring" {
+  vpc_id            = aws_vpc.atlas.id
+  service_name      = "com.amazonaws.${var.aws_region}.monitoring"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private.id
+  ]
+
+  security_group_ids = [
+    aws_security_group.vpce.id
+  ]
+
+  private_dns_enabled = true
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-monitoring-endpoint"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
